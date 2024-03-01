537edd01efb216b9adf22c3ec325b6012e3b043c
William Barrero <williambarrerocarmona@gmail.com>
2024-03-01 13:47:11 -0500

    Creación de las clases principales

---
+++ src/Persona.java
@@ -0,0 +1,11 @@
+public class Persona {
+    private String nombres, apellidos, documento;
+    private int edad;
+
+    public Persona(String nombres, String apellidos, String documento, int edad) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.documento = documento;
+        this.edad = edad;
+    }
+}
