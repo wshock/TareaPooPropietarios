93dc01370587d90bb93424993c677fe324304b42
William Barrero <williambarrerocarmona@gmail.com>
2024-03-03 18:56:05 -0500

    Creando carpeta Ventanas e img

---
+++ src/Ventanas/Ventana.java
@@ -0,0 +1,16 @@
+package Ventanas;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ventana extends JFrame implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+
+
+
+}
